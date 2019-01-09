04f3e735a61123dd4d315ffb358da6af0de15696
jheinz1999 <jonathan.heinz@aol.com>
2019-01-09 12:53:43 -0800

    GET to /api/users/:id returns correct data

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const userDB = require('./data/helpers/userDb');
 
 const server = express();
 
+const genericError = res => res.status(500).json({error: 'We were unable to process your request.'});
+
 server.use(express.json());
 
 server.get('/api/users', async (req, res) => {
@@ -18,7 +20,28 @@ server.get('/api/users', async (req, res) => {
 
   catch (err) {
 
-    res.status(500).json({error: 'We were unable to process your request.'});
+    genericError(res);
+
+  }
+
+});
+
+server.get('/api/users/:id', async (req, res) => {
+
+  try {
+
+    const user = await userDB.get(req.params.id);
+
+    if (user)
+      res.status(200).json(user);
+
+    res.status(404).json({error: 'User not found!'});
+
+  }
+
+  catch (err) {
+
+    genericError(res);
 
   }
 
