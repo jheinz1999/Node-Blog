37705a52e9bcb98e482b99af9edf3fa6cf0dc1ad
jheinz1999 <jonathan.heinz@aol.com>
2019-01-10 14:05:10 -0800

    set up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Start!</h1>
+
+        <Route
+          exact
+          path='/'
+          render={() => <h1>Slash</h1>}
+        />
+
+        <Route
+          exact
+          path='/login'
+          render={() => <h1>Log In</h1>}
+        />
+
+        <Route
+          exact
+          path='/blog'
+          render={() => <h1>Blog</h1>}
+        />
+
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
 
 import App from './App';
@@ -10,4 +12,9 @@ import './index.css';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>), document.getElementById('root'));
